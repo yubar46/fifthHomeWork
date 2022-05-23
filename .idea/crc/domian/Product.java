586e1a19ce8c314fda71242331ef54dc2e3d3366
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-05-23 14:33:27 +0430

    add to Class Product:  private String name; int number ; int price; String image; int id; int ProductTypeId; List<Feature> features;

--- .idea/crc/domian/Product.java
+++ .idea/crc/domian/Product.java
@@ -1,5 +1,15 @@
 package domian;
 
+import java.util.List;
+
 public class Product {
+
+    private String name;
+    private int number ;
+    private  int price;
+    private String image;
+    private int id;
+    private  int ProductTypeId;
+    private List<Feature> features;
 
 }
