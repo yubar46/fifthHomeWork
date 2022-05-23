fac80130c812c8eb0a1e82188f2bde44467931dc
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-05-23 14:28:49 +0430

    add to Class ProductType: get and set methods

--- .idea/crc/domian/ProductType.java
+++ .idea/crc/domian/ProductType.java
@@ -6,4 +6,28 @@ public class ProductType {
     private String name;
     private int id;
     private List<Attribute> attributes ;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Attribute> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(List<Attribute> attributes) {
+        this.attributes = attributes;
+    }
 }
